f68f45357c0850f9e014036f123863ed1971b1d3
Matias Garcia Corral <matihontayt@gmail.com>
2024-02-28 12:38:11 +0100

    menu hecho, practica terminada

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
-import EstructurasDatos.Lineales.LEG;
-import EstructurasDatos.Lineales.LEGConUltimo;
-import EstructurasDatos.Lineales.LEGConUltimoEC;
-
 public class Main {
     public static void main(String[] args)
     {
